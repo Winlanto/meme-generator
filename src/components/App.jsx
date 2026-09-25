f873b01e1ea9c1fb4c69d221d0b9c46ce1c426be
Alek-Jaan Tšern <alekjaan@gmail.com>
2026-09-25 20:33:06 +0300

    handleChange function added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,24 @@
 import "./App.css";
+import { useState } from "react";
 
 export default function App() {
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    imageUrl: "https://i.imgflip.com/1bij.jpg",
+  });
+
   function handleSubmit(e) {
     e.preventDefault();
+  }
+
+  function handleChange(e) {
+    const { name, value } = e.currentTarget;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+    console.log(value);
   }
 
   return (
@@ -16,6 +32,7 @@ export default function App() {
               id="top-text"
               name="topText"
               placeholder="One does not simply"
+              onChange={handleChange}
             />
           </div>
 
@@ -26,6 +43,7 @@ export default function App() {
               id="bottom-text"
               name="bottomText"
               placeholder="Walk into Mordor"
+              onChange={handleChange}
             />
           </div>
 
@@ -33,6 +51,11 @@ export default function App() {
             Generate Meme 🖼️
           </button>
         </form>
+        <div className="meme">
+          <img src={meme.imageUrl} alt="Meme Preview" className="meme-image" />
+          <span className="meme-text top">{meme.topText}</span>
+          <span className="meme-text bottom">{meme.bottomText}</span>
+        </div>
       </div>
     </main>
   );
